5f0e399c49909411606c528b8ad019907c73b602
FranklinJamesJones89 <fjones89@icloud.com>
2022-11-30 16:25:01 -0500

    delete default files and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={}
+              element={<Home />}
             />
           </Routes>
         </div>
