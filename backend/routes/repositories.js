b4d6d514d2e838ac2e15ed46b5a9dcf853fd29fa
FranklinJamesJones89 <fjones89@icloud.com>
2022-11-29 19:24:15 -0500

    Add all controllers

--- backend/routes/repositories.js
+++ backend/routes/repositories.js
@@ -1,37 +1,30 @@
 const express = require('express');
-const Repository = require('../models/repositoryModel')
+
+const {
+    createRepository,
+    getRepositories,
+    getRepository,
+    updateRepository,
+    deleteRepository
+} = require('../controllers/repositoryController');
+
+const Repository = require('../models/repositoryModel');
 const router = express.Router();
 
 // Get all repositories
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all repositories'});
-});
+router.get('/', getRepositories);
 
 // GET a single repository
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single repository'})
-});
+router.get('/:id', getRepository);
 
 // POST a new repository
-router.post('/', async (req, res) => {
-    const {title, synopsis, genre, form} = req.body;
+router.post('/', createRepository);
 
-    try {
-        const repository = await Repository.create({title, synopsis, genre, form});
-        res.status(200).json(repository);
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-});
 
 // DELETE a single repository
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a single repository'})
-});
+router.delete('/:id', deleteRepository);
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a single repository'})
-});
+router.patch('/:id', updateRepository);
 
 module.exports = router;
