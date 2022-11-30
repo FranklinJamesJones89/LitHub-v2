8fdb71ca0ed6695d32417fa074208d4a512042c1
FranklinJamesJones89 <fjones89@icloud.com>
2022-11-29 19:24:00 -0500

    Add all controllers for repositories

--- backend/controllers/repositoryController.js
+++ backend/controllers/repositoryController.js
@@ -1,8 +1,29 @@
 const Repository = require('../models/repositoryModel');
+const mongoose = require('mongoose');
 
 // get all repositories
+const getRepositories = async (req, res) => {
+    const repositories = await Repository.find({}).sort({createdAt: -1});
+
+    res.status(200).json({repositories});
+};
 
 // get a single repository
+const getRepository = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such repository'});
+    }
+
+    const repository = await Repository.findById(id);
+
+    if (!repository) {
+        return res.status(404).json({error: 'No such repository'});
+    } 
+
+    res.status(200).json(repository);
+};
 
 // create a new repository
 const createRepository = async (req, res) => {
@@ -18,5 +39,45 @@ const createRepository = async (req, res) => {
 };
 
 // delete a repository
+const deleteRepository = async(req, res) => {
+    const { id } = req.params
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such repository'})
+    }
+
+    const repository = await Repository.findOneAndDelete({_id: id})
+
+     if (!repository) {
+        return res.status(404).json({error: 'NO such repository'});
+     }
+
+     res.status(200).json({repository});
+}
 
 // update a repository
+const updateRepository = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such repository'});
+    }
+
+    const repository = await Repository.findOneAndUpdate({_id: id}, {
+        ...req.body
+    }) 
+
+    if (!repository) {
+        return res.status(404).json({error: 'No such repository'})
+    }
+
+    res.status(200).json({repository})
+}
+
+module.exports = {
+    createRepository,
+    getRepositories,
+    getRepository,
+    deleteRepository,
+    updateRepository
+}
