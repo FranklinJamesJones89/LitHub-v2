176f2dec4885a6fa60257850b432d8dcebd20339
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-01 17:39:06 -0500

    Create Nav component

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,36 +1,42 @@
-function Nav() {
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/"><img class="parchment-img" src="./parchment.png" alt=""/></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <Link to="/" class="nav-link active" aria-current="page">LitHub</Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/signup" class="nav-link">Sign up</Link>
-                    </li>
-                    <li>
-                        <Link to="/signup" class="nav-link">Sign in</Link>
-                    </li>
-                    <li>
-                        <Link to="/profile" class="nav-link">profile</Link>
-                    </li>
-                    <li>
-                        <Link to="/" class="nav-link">about</Link>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search LitHub" aria-label="Search"/>
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+        <div>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <Link to="/" class="navbar-brand">Navbar</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <Link to="/" class="nav-link active" aria-current="page">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="/" class="nav-link">Link</Link>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <Link to="/" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </Link>
+                                <ul class="dropdown-menu">
+                                    <li><Link to="/" class="dropdown-item">Action</Link></li>
+                                    <li><Link to="/" class="dropdown-item">Another action</Link></li>
+                                    <li><hr class="dropdown-divider" /></li>
+                                    <li><Link to="/" class="dropdown-item">Something else here</Link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     )
 };
 
