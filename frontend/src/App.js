1e05234efc8aaf603a69f970eb1bf50b49faf05e
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-03 17:02:21 -0500

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import Nav from './components/Nav';
-import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav/>
         <div className='pages'>
           <Routes>
             <Route
@@ -21,7 +18,6 @@ function App() {
               element={<Profile />}
             />
           </Routes>
-          <Main />
         </div>
       </BrowserRouter>
     </div>
