33f99b24cc33565fb35ec841ee64399f158c68aa
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-03 04:24:08 -0500

    Modify background-image in main.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Nav from './components/Nav';
+import Main from './components/Main';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
               element={<Home />}
             />
           </Routes>
+          <Main />
         </div>
       </BrowserRouter>
     </div>
