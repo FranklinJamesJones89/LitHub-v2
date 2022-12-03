78868a76d343a3292bc3b8713af192de469e0873
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-03 17:03:07 -0500

    Test

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,11 @@
+import Nav from "../components/Nav";
+import Main from "../components/Main";
+
 const Home = () => {
     return (
         <div className="home">
+            <Nav />
+            <Main />
         </div>
     )
 };
