3af9b1e0f710415b0bbe9280bd74018248afbd9d
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-03 19:14:59 -0500

    Debug

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,26 +3,26 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <Link to="/" class="navbar-brand"><img className="parchment-img" src='./parchment.png' alt="parchment"/>LitHub</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand"><img className="parchment-img" src='./parchment.png' alt="parchment"/>LitHub</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link to="/" class="nav-link nav-home" aria-current="page">Sign up</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link nav-home" aria-current="page">Sign up</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="/" class="nav-link">Sign in</Link>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">Sign in</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link to="/" class="nav-link">About</Link>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">About</Link>
                         </li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
             </div>
