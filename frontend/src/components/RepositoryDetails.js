218129612b95d53ac475b8cf86cf35af76d3caf2
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-04 18:52:40 -0500

    Remove fixed inner html and replace with props

--- frontend/src/components/RepositoryDetails.js
+++ frontend/src/components/RepositoryDetails.js
@@ -1,48 +1,54 @@
 const RepositoryDetails = ({ repository }) => {
     return (
         <div class="container-fluid">
-            <div class="row">
-                <div class="death-box col-lg-6">
+            <div class="repo-container row">
+                <div className="col-lg-6">
                     <img className="avatar" src={repository.avatar} alt="" />
                 </div>
                 <div class="col-lg-6">
-                    <div class="container px-4 py-5" id="hanging-icons">
-                        <h2 class="pb-2 border-bottom">Repoistories</h2>
-                        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    <div class="repo-container container px-4 py-5" id="hanging-icons">
+                        <h3 class="pb-2 border-bottom">Pinned</h3>
+                        <div class="repositories-box row g-4 py-5 row-cols-1 row-cols-lg-3">
                             <div class="col d-flex align-items-start">
-                                <div class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-                                    <svg class="bi" width="1em" height="1em"></svg>
-                                </div>
                                 <div>
-                                    <h3 class="fs-2">{repository.title}</h3>
-                                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                                    <a href="/" class="btn btn-primary">
-                                        Primary button
-                                    </a>
+                                    <h6 class="fs-2">{repository.title}</h6>
+                                    <p>{repository.synopsis}</p>
+                                    <p>{repository.genre}</p>
+                                    <p>{repository.form}</p>
                                 </div>
                             </div>
                             <div class="col d-flex align-items-start">
-                                <div class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-                                    <svg class="bi" width="1em" height="1em"></svg>
-                                </div>
                                 <div>
                                     <h3 class="fs-2">Featured title</h3>
                                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                                    <a href="/" class="btn btn-primary">
-                                        Primary button
-                                    </a>
                                 </div>
                             </div>
                             <div class="col d-flex align-items-start">
-                                <div class="icon-square text-bg-light d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-                                    <svg class="bi" width="1em" height="1em"></svg>
-                                </div>
                                 <div>
                                     <h3 class="fs-2">Featured title</h3>
                                     <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-                                    <a href="/" class="btn btn-primary">
-                                        Primary button
-                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="repositories-box row g-4 py-5 row-cols-1 row-cols-lg-3">
+                            <div class="col d-flex align-items-start">
+                                <div>
+                                    <h6 class="fs-2">{repository.title}</h6>
+                                    <p>{repository.synopsis}</p>
+                                    <p>{repository.genre}</p>
+                                    <p>{repository.form}</p>
+                                </div>
+                            </div>
+                            <div class="col d-flex align-items-start">
+                                <div>
+                                    <h3 class="fs-2">Featured title</h3>
+                                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                                </div>
+                            </div>
+                            <div class="col d-flex align-items-start">
+                                <div>
+                                    <h3 class="fs-2">Featured title</h3>
+                                    <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
                                 </div>
                             </div>
                         </div>
