ba734815889f9fa597092f5af59e1b172a78317d
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-03 19:14:45 -0500

    Debugging

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,19 +2,19 @@ import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
-        <div class="container-fluid main">
-            <div class="row">
-                <div class="main-txt col-lg-6">
+        <div className="container-fluid main">
+            <div className="row">
+                <div className="main-txt col-lg-6">
                     <h1>Where Stories are Told</h1>
                     <p>The platform for writers to share their work with the world</p>
-                    <div class="input-group input-group-sm">
+                    <div className="input-group input-group-sm">
                         <Link
                             to="/signup"
-                            class="main-signup span-btn input-group-text"
+                            className="main-signup span-btn input-group-text"
                             id="inputGroup-sizing-lg">
                             Sign up for LitHub
                         </Link>
-                        <input type="text" placeholder="Email Address" class="main-signup form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" />
+                        <input type="text" placeholder="Email Address" className="main-signup form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" />
                     </div>
                 </div>
             </div>
