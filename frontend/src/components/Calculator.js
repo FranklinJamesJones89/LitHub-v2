0171bb9c92dcfeb5b82bf3740add87dbde8a9eb7
FranklinJamesJones89 <fjones89@icloud.com>
2022-12-30 23:00:09 -0500

    Test

---
+++ frontend/src/components/Calculator.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const Calculator = () => {
+	const [count, setCount] = useState(4);
+
+	const decrementCount = () => {
+		setCount(prevCount => prevCount - 1)
+	}
+	
+	const incrementCount = () => {
+		setCount(prevCount => prevCount + 1)
+	}
+	
+	return (
+		<div>
+			<button onClick={decrementCount}>-</button>
+			<span>{count}</span>
+			<button onClick={incrementCount}>+</button>
+		</div>
+	)
+}
+
+export default Calculator;
--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,9 +1,14 @@
 import Nav from '../components/Nav';
 import RepositoryForm from '../components/RepositoryForm';
+import Calculator from '../components/Calculator';
+import { useState } from 'react';
 
 const Create = () => {
 	return (
+			<>
 			<RepositoryForm />
+			<Calculator />
+			</>
 	)
 }
 
